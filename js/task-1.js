7309688acd05d685ac90e838a5578d8eb471b422
Yuliia <kotyuliya24@gmail.com>
2024-03-23 18:40:49 -0400

    correction

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  let totalPrice = (quantity * pricePerDroid) > customerCredits;
+  let totalPrice = quantity * pricePerDroid;
 
   if (totalPrice > customerCredits) {
     return 'Insufficient funds!';
